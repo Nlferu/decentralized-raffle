fb4d0c25b1467a2048fb44193e471e01541a2ffc
Niferu <wargacki.patryq@gmail.com>
2022-11-24 02:16:24 +0100

    Code Cleanup

--- contracts/LotteryV2.sol
+++ contracts/LotteryV2.sol
@@ -4,17 +4,23 @@ pragma solidity ^0.8.0;
 // Entry Fee
 // If you have ticket, you can take part in lottery
 // Picking random winner based on random number
+// Same address can buy ticket multiple times, which will increase its winning chance
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-//import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LotteryV2 is VRFConsumerBaseV2, Ownable {
 
-    // Below stores all addresses that bought lottery ticket, same addresses can appear more than once, which will increase chance to win for that address.
-    // Address is payable, because we will want to pay winner whole gathered amount (corrected by commission for owners of lottery)
+    /* Lottery States */
+    enum LotteryState {
+        OPEN,
+        CALCULATING
+    }
+
+    /* Lottery Variables */
+    LotteryState private lotteryState;
     address private immutable i_owner;
     address payable[] private players;
     address private winner;
@@ -28,11 +34,10 @@ contract LotteryV2 is VRFConsumerBaseV2, Ownable {
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
-    // Variable storing minimum entrance fee.
-    uint256 private entryFee;
-    // Price feed source.
+    /* Calculating entryFee */
     AggregatorV3Interface internal price_feed;
-
+    uint256 private entryFee;
+    
     /* Errors */
     error Lottery__SendMoreToEnterLottery();
     error Lottery__LotteryNotOpen();
@@ -71,11 +76,8 @@ contract LotteryV2 is VRFConsumerBaseV2, Ownable {
         return costToEnter;
     }
 
-    function getPlayers() public view returns (address payable[] memory) {
-        return players;
-    }
-
     function getRandomNumber() public onlyOwner {
+        // ToDo: Below steps to be moved into deploy script.
         // 1. Create subscription
         // 2. Get subscription ID
         // 3. Fund subscription with LINK
@@ -85,39 +87,36 @@ contract LotteryV2 is VRFConsumerBaseV2, Ownable {
         emit RequestedLotteryWinner(requestId);
     }
 
-    // function createSub() public onlyOwner {
-    //     subsId = COORDINATOR.createSubscription();
-    // }
-
-    // function getSub() public view onlyOwner returns (uint64) {
-    //     return subsId;
-    // }
-
     // We have to override fulfillRandomWords() as it is "virtual" -> which means it expecting to be overwritten, otherwise we cant compile code.
-     function fulfillRandomWords(uint256 /* requestId */, uint256[] memory randomWords) internal override {
-        uint256 indexOfWinner = randomWords[0] % players.length;
-        address payable recentWinner = players[indexOfWinner];
-        winner = recentWinner;
+    function fulfillRandomWords(uint256 /* requestId */, uint256[] memory randomWords) internal override {
+    uint256 indexOfWinner = randomWords[0] % players.length;
+    address payable recentWinner = players[indexOfWinner];
+    winner = recentWinner;
 
-        // Transfering money to winner using call(bool sent, bytes memory data) function:
-        /* 95% of Lottery contract balance is prize for winner */
-        uint256 prize = address(this).balance * 19 / 20;
-        /* 5% of Lottery contract balance is payment for Lottery owner */
-        uint256 commission = address(this).balance * 1 / 20;
-        (bool success, ) = recentWinner.call{value: prize}("Prize For Winner Transferred!");
-        (bool sent, ) = i_owner.call{value: commission}("Commission For Lottery Owner Transferred!");
-        if (!success || !sent) {
-            revert Lottery__TransferFailed();
-        }
-        emit WinnerPicked(recentWinner);
-     }
+    // Transfering money to winner using call(bool sent, bytes memory data) function:
+    /* 95% of Lottery contract balance is prize for winner */
+    uint256 prize = address(this).balance * 19/20;
+    /* 5% of Lottery contract balance is payment for Lottery owner */
+    uint256 commission = address(this).balance * 1/20;
+    (bool success, ) = recentWinner.call{value: prize}("Prize For Winner Transferred!");
+    (bool sent, ) = i_owner.call{value: commission}("Commission For Lottery Owner Transferred!");
+    if (!success || !sent) {
+        revert Lottery__TransferFailed();
+    }
+    emit WinnerPicked(recentWinner);
+    }
+
+    function getPlayers() public view returns (address payable[] memory, uint256) {
+        uint256 players_amount = players.length;
+        return (players, players_amount);
+    }
+
+    function getPoolPrize() public view returns (uint256) {
+        uint256 prize_amount = address(this).balance * 19/20;
+        return prize_amount;
+    }
 
     function getWinner() public view returns (address) {
         return winner;
-    }
-
-    // Below function allows us as Owners of lottery to withdraw money gathered on this contract.
-    function withdraw() public payable onlyOwner {
-        payable(msg.sender).transfer(address(this).balance);
     }
 }
